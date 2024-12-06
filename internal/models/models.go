99b5ea7e7d17e29af14e1d73910694c94907105c
diceone <info@vogeler.dev>
2024-12-06 16:30:28 +0100

    new version

--- internal/models/models.go
+++ internal/models/models.go
@@ -9,14 +9,23 @@ type Task struct {
 	When        string            `json:"when,omitempty" yaml:"when,omitempty"`
 	Variables   map[string]string `json:"variables,omitempty" yaml:"variables,omitempty"`
 	Description string            `json:"description,omitempty" yaml:"description,omitempty"`
+	Env         map[string]string `json:"env,omitempty" yaml:"env,omitempty"`
 }
 
 // Playbook represents a collection of tasks
 type Playbook struct {
 	Name        string   `json:"name" yaml:"name"`
-	Description string   `json:"description,omitempty" yaml:"description,omitempty"`
+	Description string   `json:"description" yaml:"description"`
 	Hosts       []string `json:"hosts" yaml:"hosts"`
 	Tasks       []Task   `json:"tasks" yaml:"tasks"`
+}
+
+// Environment represents a collection of playbooks and their configurations
+type Environment struct {
+	Name        string               `yaml:"name" json:"name"`
+	Description string               `yaml:"description" json:"description"`
+	Variables   map[string]string    `yaml:"variables,omitempty" json:"variables,omitempty"`
+	Playbooks   map[string]Playbook  `yaml:"playbooks" json:"playbooks"`
 }
 
 // TaskResult represents the result of executing a task
